a1873c4f9410e75afa90c50a7fea22f652475260
pengcheng.yang <806321554@qq.com>
2017-09-22 23:48:47 +0800

    add an interface to get author's profile

--- controllers/me/get_my_profile.js
+++ controllers/me/get_my_profile.js
@@ -3,10 +3,14 @@ import {error} from "../../utils";
 import {AUTH_SERVER} from "../../configs";
 export default () => async ctx => {
 	try{
-		ctx.body = await (await fetch(`${AUTH_SERVER}/api/profile/${ctx.params.id}`)).json();
+		ctx.body = await (await fetch(`${AUTH_SERVER}/api/profile`, {
+			headers: {
+				Authorization: `Bearer ${ctx.cookies.get("sso_token")}`
+			}
+		})).json();
 	}catch(e){
 		ctx.body = error({
-			code: 5000200101,
+			code: 5000200100,
 			ctx,
 			e
 		});
--- controllers/me/index.js
+++ controllers/me/index.js
@@ -4,6 +4,7 @@ import body from "koa-bodyparser";
 import multer from "koa-multer";
 import validate from "../middlewares/validate";
 import {authorize} from "../middlewares";
+import getMyProfile from "./get_my_profile";
 import getProfile from "./get_profile";
 import setAvator from "./set_avator";
 import setProfile from "./set_profile";
@@ -11,7 +12,16 @@ import out from "./out";
 export default () => new Router({
 	prefix: "/api"
 })
-.get("/profile", authorize(), getProfile())
+.get("/profile", authorize(), getMyProfile())
+.get("/profile/:id", validate({
+	params: [
+		{
+			name: "id",
+			alias: "tel",
+			comment: "作者id"
+		}
+	]
+}), getProfile())
 .patch("/profile", authorize(), body(), validate({
 	body: [
 		{
