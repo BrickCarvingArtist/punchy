9ca7098a5bef28f01c8286a233b5e092d85d399c
pengcheng.yang <806321554@qq.com>
2017-10-22 23:13:37 +0800

    add a doc to the statistic middleware

--- controllers/middlewares/statistic.js
+++ controllers/middlewares/statistic.js
@@ -1,5 +1,11 @@
 import collect from "../../services/statistics";
 import {error} from "../../utils";
+/**
+ * the statistic middleware
+ * @param {String} action the collecting strategy
+ * @param {Function} then the data formatter function, which should returns an object with key value pairs
+ * @returns {undefined}
+ */
 export default (action, then) => (ctx, next) => {
 	if(!~ctx.request.header["user-agent"].indexOf("node-fetch")){
 		try{
